6e13a325e9aa594c8ba561a7459d07651033f0d3
Nataliia333 <n.o.prykhodko@gmail.com>
2021-05-11 21:31:36 +0300

    @ GET /api/contacts

---
+++ model/contacts.js
@@ -0,0 +1,24 @@
+const fs = require("fs/promises");
+const path = require("path");
+const contacts = path.join("./model", "contacts.json");
+
+const listContacts = async () => {
+  const res = await fs.readFile(contacts);
+  return res;
+};
+
+const getContactById = async (contactId) => {};
+
+const removeContact = async (contactId) => {};
+
+const addContact = async (body) => {};
+
+const updateContact = async (contactId, body) => {};
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+  updateContact,
+};
